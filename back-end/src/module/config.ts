dd3ad1c11807d8d0d337f5aee9df42708c27c372
Halex <67095898+Halex-dev@users.noreply.github.com>
2024-05-07 21:25:47 +0200

    fix docker

--- back-end/src/module/config.ts
+++ back-end/src/module/config.ts
@@ -1,9 +1,10 @@
 import * as path from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config();
+const defaultUploadPath = '/storage/uploads';
 
 export const appConfig = {
-  uploadPath:
-    path.join(process.cwd(), process.env.UPLOAD_PATH) ||
-    path.join(process.cwd(), '/storage/uploads'),
+  uploadPath: process.env.UPLOAD_PATH
+    ? path.join(process.cwd(), process.env.UPLOAD_PATH)
+    : path.join(process.cwd(), defaultUploadPath),
 };
